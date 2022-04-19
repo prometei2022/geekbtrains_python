9b87c54abd5a11a6d108b4d05511a4d0fc54d039
vladimir_mao <mao.v@mail.ru>
2022-04-20 00:04:31 +0300

    L1

--- lesson1.py
+++ lesson1.py
@@ -1,17 +1,34 @@
 #1
 
-a = 1
-b = 2
-c = 3
-print(a)
-print(a+c)
-age = input("how old are u? ")
-print(age)
+# a = 1
+# b = 2
+# c = 3
+# print(a)
+# print(a+c)
+# age = input("how old are u? ")
+# print(age)
 
 #2
-time = int(input("input time in seconds? "))
-hours = time//3600
-minutes = time%3600//60
-seconds = time%3600%60
+# time = int(input("input time in seconds? "))
+# hours = time//3600
+# minutes = time%3600//60
+# seconds = time%3600%60
+#
+# print(f"{hours:02}:{minutes:02}:{seconds:02}")
 
-print(f"clock_time = {hours:02}:{minutes:02}:{seconds:02}")
+#3
+#
+#x = input("write number n!")
+# result = int(x)+int(x*2)+int(x*3)
+# print(f"{x}+{x*2}+{x*3}={result}")
+
+#4
+
+number = int(input("get number:"))
+biggest = 0
+while number > 0:
+    tsifra = number % 10
+    if tsifra >biggest:
+        biggest = tsifra
+    number = number // 10
+print(F"the biggest number is {biggest}")
